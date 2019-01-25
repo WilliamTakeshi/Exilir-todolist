8536513c0a44a2be7345652dbca6d1f182d15fce
William Takeshi Pereira <william.takeshi@sensedata.com.br>
2019-01-25 21:44:40 -0200

    fix tests and add couple more

--- test/to_do_list_web/controllers/page_controller_test.exs
+++ test/to_do_list_web/controllers/page_controller_test.exs
@@ -3,6 +3,16 @@ defmodule ToDoListWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "Don't lose track of your goals"
+  end
+
+  test "GET /sign_in", %{conn: conn} do
+    conn = get(conn, "/sign_in")
+    assert html_response(conn, 200) =~ "react_sign_in"
+  end
+
+  test "GET /sign_up", %{conn: conn} do
+    conn = get(conn, "/sign_up")
+    assert html_response(conn, 200) =~ "react_sign_up"
   end
 end
--- test/to_do_list_web/views/error_view_test.exs
+++ test/to_do_list_web/views/error_view_test.exs
@@ -5,11 +5,11 @@ defmodule ToDoListWeb.ErrorViewTest do
   import Phoenix.View
 
   test "renders 404.json" do
-    assert render(ToDoListWeb.ErrorView, "404.json", []) == %{errors: %{detail: "Not Found"}}
+    assert render(ToDoListWeb.ErrorView, "404.json", []) == %{errors: %{detail: "Endpoint not found!"}}
   end
 
   test "renders 500.json" do
     assert render(ToDoListWeb.ErrorView, "500.json", []) ==
-             %{errors: %{detail: "Internal Server Error"}}
+             %{errors: %{detail: "Internal server error :("}}
   end
 end
